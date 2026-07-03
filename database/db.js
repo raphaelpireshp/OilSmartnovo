18c5a411f32a2c8d26f1b8eee2f1c79757d0aca4
Raphael Pires <raphaelalveshp@gmail.com>
2026-07-02 21:48:17 -0300

    trocando user do banco de dados

--- database/db.js
+++ database/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 
 const db = mysql.createConnection({
     host: 'localhost',
-    user: 'root',
+    user: 'raphael',
     password: '277353Hp', // coloque sua senha aqui
     database: 'Oil'
 });
